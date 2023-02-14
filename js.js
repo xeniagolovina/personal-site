545baaf9cb6a43f5b0a2077e98a1df44fb764568
xeniagolovina <125364667+xeniagolovina@users.noreply.github.com>
2023-02-14 17:13:41 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert(' Fear not, for I am with you; \n Be not dismayed, for I am your God; \n I will strengthen you, I will help you, \n I will uphold you with my righteous right hand.  \n Isaiah 41:10')
+})
